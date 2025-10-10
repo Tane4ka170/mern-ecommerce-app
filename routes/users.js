6f8a02d70d0c74a12e970ae7ef8833da78e1408e
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-10-10 17:37:00 +0300

    feat: Implemented auth logic with bcrypt&JWT

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const User = require("../models/UserSchema");
 
@@ -13,9 +15,45 @@ router.post("/register", async (req, res) => {
       .status(400)
       .json({ message: "User with this information already exists" });
   }
-  const newUser = new User({ email, password, name });
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new User({ email, password: hashedPassword, name });
   await newUser.save();
-  return res.status(201).json({ message: "User registered successfully" });
+
+  const token = jwt.sign({ email, id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: "12h",
+  });
+
+  return res
+    .status(201)
+    .json({ message: "User registered successfully", user: newUser, token });
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Enter your email and password" });
+  }
+
+  let user = await User.findOne({ email });
+  if (user && bcrypt.compare(password, user.password)) {
+    let token = jwt.sign({ email, id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "12h",
+    });
+    return res.status(200).json({ message: "Login successful", user, token });
+  } else {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  return res.status(200).json({ user });
 });
 
 module.exports = router;
