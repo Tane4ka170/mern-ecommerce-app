8ec1d8aeee1f76977d97badcbfb9a543fbcaf19b
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-10-12 16:02:07 +0300

    feat: Imported and rendered Highlights component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Highlights from "../components/Highlights";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Highlights />
     </div>
   );
 };
